af0cc3227ab9be2f616804fda470a2bedbc852b5
Arturo Pie <arturotd08@yahoo.ca>
2013-11-21 22:48:10 -0500

    adds teporingo dependency and some WIP

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject gorgon-visio "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Tool for visualizing gorgon runs"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.github.kyleburton/teporingo "2.1.27"]]
   :main ^:skip-aot gorgon-visio.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/gorgon_visio/core.clj
+++ src/gorgon_visio/core.clj
@@ -1,7 +1,43 @@
 (ns gorgon-visio.core
-  (:gen-class))
+  (:gen-class)
+  (:require [teporingo.broker :as broker]
+            [clj-etl-utils.log :as log])
+  ;; (:use
+  ;;  [teporingo.core   :only [*reply-code* *reply-text* *exchange* *routing-key* *message-properties* *listener* *conn* *props* *body*
+  ;;                           *active* *confirm-type* *delivery-tag* *multiple* publisher publish]]
+   ;; [clj-etl-utils.lang-utils :only [raise]])
+  )
+
+;; (defn handle-returned-message []
+;;   (log/errorf
+;;    "[publisher] RETURNED: conn=%s code=%s text=%s exchange=%s routing-key:%s props=%s body=%s"
+;;    @*conn*
+;;    *reply-code*
+;;    *reply-text*
+;;    *exchange*
+;;    *routing-key*
+;;    *props*
+;;    (String. *body*)))
+
+(broker/register
+ :amqp01
+ {:name               :amqp01
+  :roles              #{:local}
+  ;; :user              "guest"
+  ;; :pass              "guest"
+  ;; :host              "localhost"
+  :port               25671
+  :connection-timeout 10
+  :reconnect-delay-ms 1000
+  ;; :heartbeat-seconds 1
+  :vhost              "/"
+  ;; :use-confirm        false
+  ;; :basic-qos          {:prefetch-size 0 :prefetch-count 1}
+  ;; :use-transactions   false
+  ;; :auto-ack           false
+  :listeners          {:return  handle-returned-message}})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Changed!"))
